edbb904839076ecd5b9fde150e9ee249797c37ca
Krzysztof Sakowski <krz.sakowski@gmail.com>
2017-11-02 20:42:30 +0100

    Added django hello world.

---
+++ project/production_settings.py
@@ -0,0 +1,18 @@
+from project.settings import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'lunchcrawler.heroku.com',
+]
+
+#CSRF_COOKIE_SECURE = True
+#SESSION_COOKIE_SECURE = True
+
+#X_FRAME_OPTIONS = 'DENY'
+#CSRF_COOKIE_HTTPONLY = True
+#SECURE_SSL_REDIRECT = True
+#SECURE_BROWSER_XSS_FILTER = True
+#SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# TODO SECURE_HSTS_SECONDS
