f6c561b461feca9665c14213ae13c3b6b683e081
Krzysztof Sakowski <krz.sakowski@gmail.com>
2017-11-08 14:16:42 +0100

    Fix bug. Not everything on facebook wall contains message.

--- lunch/views.py
+++ lunch/views.py
@@ -28,7 +28,6 @@ restaurants = [
     Restaurant(name="Welldone", facebook_name="welldonekrakow"),
     Restaurant(name="Bal", facebook_name="balnazablociu"),
     Restaurant(name="Nasze Smaki Bistro", facebook_name="405341849804282"),
-    Restaurant(name="Nasze Smaki", facebook_name="1451080808450048"),
     Restaurant(name="PapaYo", facebook_name="papayokrakow"),
     Restaurant(name="Mniam", facebook_name="mniamkrakow"),
 ]
@@ -39,6 +38,33 @@ facebook_app_secret = getattr(settings, "FACEBOOK_APP_SECRET", None)
 access_token = facebook_app_id + "|" + facebook_app_secret
 
 
+def get_menu(restaurant, posts):
+
+    if not posts:
+        return Menu(restaurant.name, "", 0, "", "", is_today_menu=False)
+
+    post = posts[0]
+
+    menu = post['message']
+    post_id = post['id']
+    created_time = post['created_time']
+
+    date = dateutil.parser.parse(created_time)
+
+    to_zone = tz.gettz('Europe/Warsaw')
+    date_polish = date.astimezone(to_zone)
+
+    date_now_polish = datetime.datetime.now()
+    date_now_polish = date_now_polish.astimezone(to_zone)
+
+    if restaurant.name == "PapaYo":
+        menu = menu.replace("\n\n", "\n")
+
+    is_today_menu = date_polish.day == date_now_polish.day
+
+    return Menu(restaurant.name, menu, post_id, created_time, f"{date_now_polish:%d}", is_today_menu=is_today_menu)
+
+
 def index(request):
     graph = facebook.GraphAPI(access_token=access_token)
 
@@ -47,27 +73,13 @@ def index(request):
     for restaurant in restaurants:
         user = restaurant.facebook_name
         profile = graph.get_object(user)
-        posts = graph.get_connections(profile['id'], 'posts')
+        posts = graph.get_connections(profile['id'], 'posts')['data']
 
-        menu = posts['data'][0]['message']
-        post_id = posts['data'][0]['id']
-        created_time = posts['data'][0]['created_time']
-
-        date = dateutil.parser.parse(created_time)
-
-        to_zone = tz.gettz('Europe/Warsaw')
-        date_polish = date.astimezone(to_zone)
-
-        date_now_polish = datetime.datetime.now()
-        date_now_polish = date_now_polish.astimezone(to_zone)
-
-        if restaurant.name == "PapaYo":
-            menu = menu.replace("\n\n", "\n")
-
-        is_today_menu = date_polish.day == date_now_polish.day
+        # filter posts that does not contain message
+        posts = [post for post in posts if 'message' in post]
 
         menus.append(
-            Menu(restaurant.name, menu, post_id, created_time, f"{date_now_polish:%d}", is_today_menu=is_today_menu)
+            get_menu(restaurant=restaurant, posts=posts)
         )
 
     context = {
