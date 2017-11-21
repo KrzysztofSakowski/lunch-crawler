a08bde95ab8d871629444fa89b9e85b43e779ced
Krzysztof Sakowski <krz.sakowski@gmail.com>
2017-11-21 23:08:16 +0100

    Removed redundant production setting for heroku

--- project/production_settings.py
+++ project/production_settings.py
@@ -2,22 +2,13 @@ from project.settings import *
 
 DEBUG = False
 
-if 'HEROKU_APP_NAME' in os.environ:
-    ALLOWED_HOSTS = [
-        os.environ['HEROKU_APP_NAME'] + '.herokuapp.com',
-    ]
-else:
-    ALLOWED_HOSTS = [
-        'lunchcrawler.herokuapp.com',
-    ]
+SECURE_SSL_REDIRECT = True
 
 #CSRF_COOKIE_SECURE = True
 #SESSION_COOKIE_SECURE = True
 
 #X_FRAME_OPTIONS = 'DENY'
 #CSRF_COOKIE_HTTPONLY = True
-
-SECURE_SSL_REDIRECT = True
 
 #SECURE_BROWSER_XSS_FILTER = True
 #SECURE_CONTENT_TYPE_NOSNIFF = True
