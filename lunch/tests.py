9df8f223d52e2ae3f043df3668ae438aa6aa6bb5
damck <damian.goworko@gmail.com>
2018-01-05 19:36:42 +0000

    Add tests for restaurant duplication in user profile

--- lunch/tests.py
+++ lunch/tests.py
@@ -6,9 +6,10 @@ import dateutil.parser
 from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 from .facebook_api import Facebook
-from .models import Restaurant, FacebookPost
+from .models import Restaurant, FacebookPost, UserProfile
 from .views import find_in_db, crawl_facebook
 
 
@@ -80,7 +81,9 @@ class IndexViewTests(TestCase):
     def test_response(self):
         response = self.client.get(reverse("index"))
 
-        self.assertEqual(200, response.status_code)
+        # temporary fix while main page redirects
+        # self.assertEqual(200, response.status_code)
+        self.assertEqual(302, response.status_code)
 
     def test_db_find(self):
         restaurant = Restaurant.objects.get(id=1)
@@ -127,3 +130,25 @@ class IndexViewTests(TestCase):
     def test_get_id(self):
         facebook = Facebook()
         self.assertEqual("1550000475309485", facebook.get_facebook_id("zustdoustkrk"))
+
+
+class UserProfileTests(TestCase):
+    def test_profile_adds_restaurants(self):
+        userprofile = UserProfile.objects.create(user=User.objects.create(username='asd', password='1234'))
+        restaurant1 = Restaurant.objects.create(name='Res1', facebook_id='1')
+        restaurant2 = Restaurant.objects.create(name='Res2', facebook_id='2')
+
+        userprofile.restaurants.add(restaurant1)
+        userprofile.restaurants.add(restaurant2)
+
+        self.assertEqual(len(userprofile.restaurants.all()), 2)
+
+    def test_profile_validation_for_duplicate_restaurants(self):
+        userprofile = UserProfile.objects.create(user=User.objects.create(username='asd', password='1234'))
+        restaurant = Restaurant.objects.create(name='Res1', facebook_id='1')
+
+        userprofile.restaurants.add(restaurant)
+        userprofile.restaurants.add(restaurant)
+
+        self.assertEqual(len(userprofile.restaurants.all()), 1)
+
